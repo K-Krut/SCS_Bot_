ea831b301453013e81a9d5560c01bc41c4eb8b76
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-29 18:11:37 +0200

    adding functional

--- AirtableParsing.py
+++ AirtableParsing.py
@@ -1,50 +1,37 @@
-# "OrderedDict([('id', 'rec1Ed9tKDZKd6xDb'), ('fields', OrderedDict([('Status', '🧑🏼\u200d💼 SM Editing Review'), " \
-# "('Name', 'TiSo - Football event photos | Basic Photo Editing (<100 RAW Photos) | TiSo rec1Ed9tKDZKd6xDb'), " \
-# "('Brand', ['Titanium Solar'])])), ('createdTime', '2021-12-20T19:17:03.000Z')])".encode('utf-8', 'namereplace')
-
-# str.join('\n', [f'{i}: {self.__dict__[i]}' for i in self.__dict__.keys()])
-# def __str__(self):
-#      return '\n'.join([''.join([f'{j}: {i[j]}\n' for j in i.keys()]) for i in data.values()])
-from typing import NamedTuple
-
+import json
 import airtable
+from exceptions import SearchUser
 from config import BASE_ID, API_KEY
+from Constants import TABLE
+from Constants import TASKS_IN_PROGRESS_FORMULA, FIELDS
 
-
-TABLE = '💻 Editing'
-view_ = 'Vlad Legun In Progress'
-FORMULA_1 = "AND({Status} != '✅ Editing Done', {Status} != '🤴🏻 Client Editing Approval')"
-FORMULA_2 = "OR({Status} = '👨🏻‍💻 Editor Assigned', {Status} = '✋🏼 On Hold')"
 
 Data_ = airtable.Airtable(BASE_ID, API_KEY)
-DICT_ = Data_.iterate(
-    table_name=TABLE, view=view_, fields=['Name', 'Status', 'Brand'],
-    filter_by_formula=FORMULA_2, max_records=5
-)
 
 
-class Records(NamedTuple):
-    name_ = str
-    status_ = str
-    brand_ = str
+def get_user_view(user_id):
+    data = json.load(open('Creators.json'))
+    if not str(user_id) in data:
+        raise SearchUser(user_id)
+    print(data[str(user_id)]['view'])
+    return data[str(user_id)]['view']
 
 
-#
-# def getting_data():
-#     for i in DICT_:
-#         print(i.get('fields').values())
-#         for j in i.get('fields').items():
-#             print(type(j))
-#             processing(j)
-#
-
-
-def getting_data():
-    return [processing(i.get('fields')) for i in DICT_]
+def get_view_data(user_id):
+    return Data_.iterate(
+        table_name=TABLE, view=get_user_view(user_id), fields=FIELDS,
+        # filter_by_formula=TASKS_IN_PROGRESS_FORMULA,
+    )
 
 
 def processing(data):
+    print(data)
     return f'<b><i>{data["Name"].split(" | ")[0]}</i></b>\n' \
            f'Status: {data["Status"].replace("u200d", "")}\n' \
            f'Brand: {data["Brand"][0]}'
+
+
+def getting_processed_data(user_id):
+    print([processing(i.get('fields')) for i in get_view_data(user_id)])
+    return [processing(i.get('fields')) for i in get_view_data(user_id)]
 
