a4ba791a82f0c18758fc72b15fdf3fa9fe362663
kekekek <90685950+K-Krut@users.noreply.github.com>
2022-03-03 11:19:54 +0200

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -30,26 +30,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-#
-# @dp.message_handler(commands=['start'])
-# async def send_welcome_message(message: types.Message):
-#     await message.delete()
-#     await bot.send_message(
-#         message.from_user.id, f"👤*Hi! {message.from_user.first_name if message.from_user.first_name else ''} "
-#                               f"{message.from_user.last_name if message.from_user.last_name else ''}\n "
-#                               f"I'm bot Assistant.*", parse_mode="HTML",
-#         reply_markup=markup.keyboard_
-#     )
-#
-#
-# @dp.callback_query_handler(text='TASKS_IN_PROGRESS')
-# async def tasks_in_progress_handler(call: types.CallbackQuery):
-#     await call.message.delete()
-#     try:
-#         await bot.send_message(
-#             call.from_user.id,
-#             '\n\n'.join([i for i in AirtableParsing.getting_processed_data(call.from_user.id)]),
-#             parse_mode='HTML'
-#         )
-#     except Exception as exp:
-#         return
+
