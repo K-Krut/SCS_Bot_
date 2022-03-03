c49fd2f3ba830afa50ee5ede1c1a75e7832dd1e2
kekekek <90685950+K-Krut@users.noreply.github.com>
2022-03-03 13:35:46 +0200

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -2,10 +2,10 @@ from datetime import datetime
 import schedule as schedule
 import time
 import AirtableParsing
-import config
 import telegram
-import markup
-bot = telegram.Bot(token=config.TOKEN)
+from config import TOKEN
+
+bot = telegram.Bot(token=TOKEN)
 
 
 def handler():
@@ -15,7 +15,7 @@ def handler():
             for i in records:
                 bot.send_message(chat_id=i[0], text=i[1], parse_mode="HTML")
     except Exception as exp:
-        print("Failed at", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        print("Failed at", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '\n', exp)
 
 
 def main():
@@ -30,4 +30,26 @@ def main():
 if __name__ == '__main__':
     main()
 
-
+#
+# @dp.message_handler(commands=['start'])
+# async def send_welcome_message(message: types.Message):
+#     await message.delete()
+#     await bot.send_message(
+#         message.from_user.id, f"👤*Hi! {message.from_user.first_name if message.from_user.first_name else ''} "
+#                               f"{message.from_user.last_name if message.from_user.last_name else ''}\n "
+#                               f"I'm bot Assistant.*", parse_mode="HTML",
+#         reply_markup=markup.keyboard_
+#     )
+#
+#
+# @dp.callback_query_handler(text='TASKS_IN_PROGRESS')
+# async def tasks_in_progress_handler(call: types.CallbackQuery):
+#     await call.message.delete()
+#     try:
+#         await bot.send_message(
+#             call.from_user.id,
+#             '\n\n'.join([i for i in AirtableParsing.getting_processed_data(call.from_user.id)]),
+#             parse_mode='HTML'
+#         )
+#     except Exception as exp:
+#         return
