d66580847764affa54501274315ebb00db21eade
kekekek <katiakrut448@gmail.com>
2025-01-06 22:46:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-TOKEN = '5142508075:AAH_zS9_P_-Ng6VG4HYVFZZ7BsxzOidlphc'
-# TOKEN = '5142508075:AAH_zS9_P_-Ng6VG4HYVFZZ7BsxzOidlphc'
-# TOKEN = '5142508075:AAH_zS9_P_-Ng6VG4HYVFZZ7BsxzOidlphc'
+TOKEN = ''
 
-
-API_KEY = 'keytmiDZHJP7bNZby'
-BASE_ID = 'app8dUiwvFFBqZwct'
+API_KEY = 'key'
+BASE_ID = 'app'
 
 
 
